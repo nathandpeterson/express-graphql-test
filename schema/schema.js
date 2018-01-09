b2cef9dee6cdd34d71b2db4b9edabc86ea109805
Nathan Daniel Peterson <natperson@gmail.com>
2018-01-09 06:37:16 -0800

    add patch and delete mutations to schema

--- schema/schema.js
+++ schema/schema.js
@@ -6,7 +6,8 @@ const {
     GraphQLString,
     GraphQLInt,
     GraphQLSchema,
-    GraphQLList
+    GraphQLList,
+    GraphQLNonNull
 } = graphql
 
 const CompanyType = new GraphQLObjectType({
@@ -63,6 +64,50 @@ const RootQuery = new GraphQLObjectType({
     }
 })
 
+const mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+      addUser: {
+          type: UserType,
+          args: { 
+                firstName: {type: new GraphQLNonNull(GraphQLString)},
+                age: {type: new GraphQLNonNull(GraphQLInt)},
+                companyId: {type: GraphQLString}
+          },
+          resolve(parentValue, {firstName, age}){
+            return axios.post(`http://localhost:3000/users`, {
+                firstName,
+                age
+            }).then(response=> response.data)
+          }
+      },
+      deleteUser: {
+          type: UserType,
+          args: {
+              id: {type: new GraphQLNonNull(GraphQLString)}
+          },
+          resolve(parentValue, args){
+            return axios.delete(`http://localhost:3000/users/${args.id}`)
+                .then(response => response.data)
+          }
+      },
+      editUser: {
+          type: UserType,
+          args: {
+            id: {type: new GraphQLNonNull(GraphQLString)},
+            firstName: {type: GraphQLString},
+            age: {type: GraphQLInt},
+            companyId: {type: GraphQLString}
+          },
+          resolve(parentValue, args) {
+              return axios.patch(`http://localhost:3000/users/${args.id}`, args)
+              .then(response => response.data)
+          }
+      }  
+    }
+})
+
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation
 })
